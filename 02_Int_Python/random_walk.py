3db99c3f181774679171c2c515e1a733aa7fc44e
lingyenlee <lingyenlee@gmail.com>
2019-08-28 07:14:36 +0200

    first commit

---
+++ 02_Int_Python/random_walk.py
@@ -0,0 +1,32 @@
+# Numpy is imported, seed is set
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(123)
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100):
+    step = random_walk[-1]
+    dice = np.random.randint(1, 7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1, 7)
+
+    random_walk.append(step)
+
+print(random_walk)
+
+# Import matplotlib.pyplot as plt
+
+# plot x (range 1-100) against random_walk
+# Plot random_walk
+plt.plot(random_walk)
+
+# Show the plot
+plt.show()
---
+++ 04_DS_Toolbox_2/chunk.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+data = pd.read_csv("tweets.csv", chunksize=10)
+
+# Initialize an empty dictionary: counts_dict
+counts_dict = {}
+
+# Iterate over the file chunk by chunk
+for chunk in data:
+
+    # Iterate over the column in DataFrame
+    for entry in chunk['lang']:
+        if entry in counts_dict.keys():
+            counts_dict[entry] += 1
+        else:
+            counts_dict[entry] = 1
+
+# Print the populated dictionary
+print(counts_dict)
